0ffed8579f536b147b31afb8e090a345236e83e8
FlavioGP <flavio.gpinheiro@hotmail.com>
2025-01-28 09:18:48 -0300

    chore: initial configs

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Container from "./components/container";
+import Form from "./components/form";
+import Result from "./components/result";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Form />
+        <Result />
+      </Container>
+    </>
+  );
+}
+
+export default App;
